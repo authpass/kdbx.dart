a9a65adfb53baa1bdc01e7f4ec2d4878a8b69031
Herbert Poul <herbert@codeux.design>
2021-05-22 12:34:03 +0200

    do something useful in the example.

--- example/kdbx_example.dart
+++ example/kdbx_example.dart
@@ -1,5 +1,14 @@
 import 'package:kdbx/kdbx.dart';
 
 void main() {
-  KdbxFormat().read(null, null);
+  final kdbx = KdbxFormat()
+      .create(Credentials(ProtectedValue.fromString('Lorem Ipsum')), 'Example');
+
+  final group = kdbx.body.rootGroup;
+  final entry = KdbxEntry.create(kdbx, group);
+  group.addEntry(entry);
+  entry.setString(KdbxKeyCommon.USER_NAME, PlainValue('example user'));
+  entry.setString(
+      KdbxKeyCommon.PASSWORD, ProtectedValue.fromString('password'));
+  kdbx.save();
 }
