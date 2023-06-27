c9f82049d265118afecf394115360d219185ad6e
Waqar Ahmed <waqar.17a@gmail.com>
2023-06-27 16:59:07 +0500

    Recognize twoFish algorithm
    Not supported, but at least it gives a better in the exception / error
    now

--- lib/src/internal/consts.dart
+++ lib/src/internal/consts.dart
@@ -8,12 +8,16 @@ enum Cipher {
 
   /// Support since kdbx 4.
   chaCha20,
+
+  /// Not Supported, just recognized
+  twoFish
 }
 
 class CryptoConsts {
   static const CIPHER_IDS = <Cipher, KdbxUuid>{
     Cipher.aes: KdbxUuid('McHy5r9xQ1C+WAUhavxa/w=='),
     Cipher.chaCha20: KdbxUuid('1gOKK4tvTLWlJDOaMdu1mg=='),
+    Cipher.twoFish: KdbxUuid('rWjyn1dvS7mjatR6+WU0bA=='),
   };
   static final cipherByUuid =
       CIPHER_IDS.map((key, value) => MapEntry(value, key));
